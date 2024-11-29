833f289a628650c2e5fcc6ceca432867c9647440
Phoebe Chang <phoebe03111@gmail.com>
2024-11-29 13:58:03 -0800

    Create docker image

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,10 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+  // Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Missing input or output file path.");
+  }
+
+  // Convert video to 360p
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Video processing finished successfully.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occured: ${err.message}`);
+      res.send(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
